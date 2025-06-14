7a54846854eca2e8bd3539c57095087102db1260
sergzv-dev <serg.zv.dev@gmail.com>
2025-06-15 03:04:38 +0400

    Adjustment maze generator

--- Maze_generator.py
+++ Maze_generator.py
@@ -1,12 +1,11 @@
 import copy, random
-import itertools as it
 
 arr_gen = lambda x, y: [[0 for _ in range(x)] for _ in range(y)]
 arr_print = lambda matrix: [print(row) for row in matrix]
 
 def maze_gen(x_line, y_line):
     matrix = arr_gen(x_line, y_line)
-    left_border = copy.deepcopy(matrix)
+    right_border = copy.deepcopy(matrix)
     under_border = copy.deepcopy(matrix)
     under_chek = 0
     array_chek = 0
@@ -16,7 +15,7 @@ def maze_gen(x_line, y_line):
 
         for num_i, itm in enumerate(line):
             if random.randint(1,3) == 1:
-                left_border[num_l][num_i] = 1
+                right_border[num_l][num_i] = 1
                 array_interrupt = True
             else: array_chek += 1
 
@@ -32,17 +31,17 @@ def maze_gen(x_line, y_line):
                 array_interrupt = False
 
             if num_i == len(line) - 1:
-                left_border[num_l][num_i] = 1
+                right_border[num_l][num_i] = 1
 
         if num_l == len(matrix) - 1:
             under_border[num_l] = [1] * x_line
 
-    return left_border, under_border
+    return right_border, under_border
 
 
-left_border, under_border = maze_gen(10, 15)
+right_border, under_border = maze_gen(10, 15)
 
-print('\nleft_border\n')
-arr_print(left_border)
+print('\nright_border\n')
+arr_print(right_border)
 print('\nunder_border\n')
 arr_print(under_border)
