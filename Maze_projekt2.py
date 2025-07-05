35747a4730291a93bd12abbf968703e3dc804355
sergzv-dev <serg.zv.dev@gmail.com>
2025-07-05 13:15:53 +0400

    Questions

--- Maze_projekt2.py
+++ Maze_projekt2.py
@@ -1,4 +1,4 @@
-class UI():  #может есть смысл все методы UI сделать @statikmetod, чтоб использовать в любой части программы и не париться?
+class UI():
     def ask(self, text):
         answer = input(text)
         return answer
@@ -15,8 +15,8 @@ def game():
     master = UI()
     name = master.ask("What is your name? ")
     stranger = Player(name)
-    world = map_builder(10, 10)
-    curr_room = world['00']
+    world = World()
+    curr_room = world.rooms_dict[name_convert('A1')]
     game_state = GameState(master, world, stranger, curr_room)
     while True:
         actions = game_state.possible_actions()
@@ -41,7 +41,7 @@ class Room():
         MoveAction(self)
 
     def __repr__(self):
-        return self.name
+        return name_convert(self.name)
 
 
 class Action():
@@ -62,12 +62,14 @@ class MoveAction(Action):
         self.state = game_state
 
     def __repr__(self):  #хочу сделать repr чтоб печаталось "выбрать путь" но нет доступа к UI на данном этапе
-        pass
+        return f'Go to the room {self.room}'
 
 
 class OpenBackPack:
     def show(self):
         return self.state.player.back_pack
+    def __repr__(self):
+        return 'Open back pack'
 
 class GetItem():
     def get_it(self):
@@ -88,16 +90,44 @@ class Creature():
         self.back_pack = []
         self.actions = []
 
+
 class Player(Creature):
+    def __init__(self, name):
+        super().__init__(name)
+        self.actions = [OpenBackPack]
 
 class Monster(Creature):
+    pass
 
-def map_builder(x_line, y_line):
+class World():
+    def __innit__(self, x_line, y_line):
+        self.size = (x_line, y_line)
+        self.x_line = x_line
+        self.y_line = y_line
+        self.rooms_dict = dict()
+        self.map_builder()
+
+    def map_builder(self, func):
+        for x in range(self.x_line):
+            self.rooms_dict.update({(x, y): func((x, y)) for y in range(self.y_line)})
+
+
+def name_convert(x, y = None):
+    res = None
+    if isinstance(x, int):
+        res = f'{chr(x+64)}{str(y)}'
+    if isinstance(x, str):
+        res = ord(x[0])-64, int(x[1])
+    return res
+
+
+
+'''def map_builder(x_line, y_line):
     rooms_dict = {'map_size': (x_line, y_line)}  #что думаешь если зашить map_size в rooms_dict как тех.инфу чтоб достать из GameState
     name = lambda x, y: f'{str(x)}{str(y)}'
     for x in range(x_line):
         rooms_dict.update({name(x,y): Room(name(x,y)) for y in range(y_line)})
-    return rooms_dict
+    return rooms_dict'''
 
 
 game()
