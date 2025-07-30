797ce38c387b03e43f4dfcbc73862eac20633a07
sergzv-dev <serg.zv.dev@gmail.com>
2025-07-31 01:15:27 +0400

    bug fix

--- ui_terminal.py
+++ ui_terminal.py
@@ -7,8 +7,15 @@ class UI():
 
     def choose(self, options):
         create_opt = '\n'.join(f'{i}: {num + 1}' for num, i in enumerate(options))
-        answer = input(f'{create_opt}\n...')
-        return options[int(answer)-1]
+        while True:
+            answer = self.ask(f'{create_opt}\n...')
+            try:
+                if 0 < int(answer) <= len(options):
+                    return options[int(answer)-1]
+                else: raise ValueError
+            except ValueError:
+                self.say('Darkness envelops your mind.. Try again!\n')
+                continue
 
     def say(self, text):
         print(text)
