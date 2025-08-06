06b59d5caba897ce3943053ced9828790c7f9fb2
sergzv-dev <serg.zv.dev@gmail.com>
2025-08-07 02:45:42 +0400

    bug fix

--- tests/test_end_game.py
+++ tests/test_end_game.py
@@ -6,24 +6,25 @@ from room import Room
 from treasures import Key
 from ui_terminal import UI
 
-test_player = Player('test')
-test_room = Room((1,1))
-test_key = Key('test key')
-wrong_key = Key('wrong')
-test_ui = UI()
-test_action = EndDoorAction(test_key)
-game_states = GameState(master = test_ui, world = None, player = test_player, curr_room = test_room)
+def test():
+    test_player = Player('test')
+    test_room = Room((1,1))
+    test_key = Key('test key')
+    wrong_key = Key('wrong')
+    test_ui = UI()
+    test_action = EndDoorAction(test_key)
+    game_states = GameState(master = test_ui, world = None, player = test_player, curr_room = test_room)
 
-game_states.curr_room.actions.append(test_action)
+    game_states.curr_room.actions.append(test_action)
 
-game_states.player.back_pack = [test_key]
-test_action.execute(game_states)
+    game_states.player.back_pack = [test_key]
+    test_action.execute(game_states)
 
-assert isinstance(test_action.execute(game_states), HappyEnd)
-assert isinstance(test_action.execute(game_states), EndGame)
+    assert isinstance(test_action.execute(game_states), HappyEnd)
+    assert isinstance(test_action.execute(game_states), EndGame)
 
-game_states.player.back_pack = [wrong_key]
-assert isinstance(test_action.execute(game_states), GameState)
+    game_states.player.back_pack = [wrong_key]
+    assert isinstance(test_action.execute(game_states), GameState)
 
-game_states.player.back_pack = [None]
-assert isinstance(test_action.execute(game_states), GameState)
+    game_states.player.back_pack = [None]
+    assert isinstance(test_action.execute(game_states), GameState)
