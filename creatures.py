1ab11b1af019acf4933d5edb1cc7776afd433357
sergzv-dev <serg.zv.dev@gmail.com>
2025-08-07 20:55:58 +0400

    add specs changer

--- creatures.py
+++ creatures.py
@@ -7,9 +7,28 @@ class Creature():
         self.name = name
         self.attack = attack
         self.shield = shield
+        self.max_shield = 25
         self.hp = hp
+        self.max_hp = 100
         self.agility = agility
 
+    def heal_hp(self, value):
+        self.hp = min(self.max_hp, self.hp + value)
+
+    def get_damage(self, value, *, death = True):
+        min_hp = -1
+        if not death: min_hp = 1
+        self.hp = max(min_hp, self.hp - value)
+
+    def increase_spec(self, spec, value):
+        max_val = float('inf')
+        if spec == 'shield': max_val = self.max_shield
+        if spec == 'hp': max_val = self.max_hp
+        setattr(self, spec, min(max_val, getattr(self, spec) + value))
+
+    def reduce_spec(self, spec, value):
+        min_val = 1
+        setattr(self, spec, max(min_val, getattr(self, spec) - value))
 
 class Player(Creature):
     def __init__(self, name, attack = 10, shield = 20, hp = 100, agility = 5):
